91257d0a6916d01f66e01faacba0d46ec2389d7f
santos <developerh69@gmail.com>
2017-10-16 18:13:28 -0500

    modifique el archivo Post.php, creando un metodo para cargar posts

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -31,5 +31,25 @@ class Post{
 			$update_query = mysqli_query($this->con, "UPDATE users SET num_posts = '$num_posts' WHERE username = '$added_by'");
 		}
 	}
+
+	public function loadPostsFriends(){
+		$str = "";
+		$data = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted = 'no' ORDER BY id DESC");
+		while ($row = mysqli_fetch_array($data)) {
+			$id = $row['id'];
+			$body = $row['body'];
+			$added_by = $row['added_by'];
+			$date_time = row['date_added'];
+
+			//Prepare user_to string so it can be included even if not posted to a user
+			if ($row['user_to'] == "none") {
+				$user_to = "";
+			}else{
+				$user_to_obj = new User($con, $row['user_to']);
+				$user_to_name = $user_to_obj->getFirstAndLastName();
+				$user_to = "<a href='" . $row['user_to'] . "'>" . $user_to_name . "</a>";
+			}
+		}
+	}
 }
 ?>
