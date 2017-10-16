af4d638f9784a6f5e2d51b8886d7db2f73da9bba
santos <developerh69@gmail.com>
2017-10-16 17:13:47 -0500

    Archivo Post.php, insert posts, update users campo num_posts, Archivo User.php agrege metodo getnumposts, en index.php creo objeto post y creo archivo logout.php

--- includes/header.php
+++ includes/header.php
@@ -31,6 +31,7 @@
 			<a href="#"><i class="fa fa-bell-o fa-lg"></i></a>
 			<a href="#"><i class="fa fa-users fa-lg"></i></a>
 			<a href="#"><i class="fa fa-cog fa-lg"></i></a>
+			<a href="includes/handlers/logout.php"><i class="fa fa-sign-out fa-lg"></i></a>
 		</nav>
 	</div>
 	<div class="wrapper">
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php 
 include("includes/header.php");
 include("includes/classes/User.php");
+include("includes/classes/Post.php");
+
+if (isset($_POST['post'])) {
+	$post = new Post($con, $userLoggedIn);
+	$post->submitPost($_POST['post_text'], 'none');
+}
 ?>
 	<div class="user_details column">
 		<a href="<?php echo $userLoggedIn; ?>"><img src="<?php echo $user['profile_pic'];?>"></a>
