d5e0c78750ef171535112655b0ef50652d0cddb7
santos <developerh69@gmail.com>
2017-12-11 19:59:48 -0500

    Mostrando la seccion comentarios y posteando nuestro primer comentario

--- comment_frame.php
+++ comment_frame.php
@@ -44,6 +44,7 @@ if (isset($_POST['postComment' . $post_id])) {
 	$post_body = mysqli_escape_string($con, $post_body);
 	$date_time_now = date("Y-m-d H:i:s");
 	$insert_post = mysqli_query($con, "INSERT INTO comments VALUES('', '$post_body', '$userLoggedIn', '$posted_to', '$date_time_now', 'no', '$post_id')");
+	echo "<p>Comment Posted!</p>";
 }
 
 ?>
--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -88,6 +88,18 @@ class Post{
 					$last_name = $user_row['last_name'];
 					$profile_pic = $user_row['profile_pic'];
 
+					?>
+					<script>
+						function toggle<?php echo $id; ?>(){
+							var element = document.getElementById("toggleComment<?php echo $id; ?>");
+							if (element.style.display == "block")
+								element.style.display = "none";
+							else
+								element.style.display = "block";
+						}
+					</script>
+					<?php
+
 					//TimeFrame
 					$date_time_now = date("Y-m-d H:i:m");
 					$start_date = new DateTime($date_time); //Time of post
@@ -131,7 +143,7 @@ class Post{
 							$time_message = $interval->s . " seconds ago";
 					}
 
-					$str .= "<div class='status_post'>
+					$str .= "<div class='status_post' onClick='javascript:toggle$id()'>
 								<div class='post_profile_pic'>
 									<img src='$profile_pic' width='50'>
 								</div>
@@ -142,6 +154,9 @@ class Post{
 									$body
 									<br>
 								</div>
+							</div>
+							<div class='post_comment' id='toggleComment$id' style='display:none;'>
+								<iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0'></iframe>
 							</div>
 							<hr>";
 				}
