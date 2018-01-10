ce75f401f902c833410d232d398f2b9a99fb7c8d
santoshm <dosantoshm@gmail.com>
2018-01-09 19:33:56 -0500

    Creating the left profile bar

--- profile.php
+++ profile.php
@@ -1,11 +1,25 @@
 <?php 
 include("includes/header.php");
+
+if (isset($_GET['profile_username'])) {
+	$username = $_GET['profile_username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+	$user_array = mysqli_fetch_array($user_details_query);
+
+	$num_friends = (substr_count($user_array['friend_array'], ",")) - 1; //Contar el número de cadenas repetidas
+}
 ?>
-	<div class="main_column column">
-		this is a profile
+	<div class="profile_left">
+		<img src="<?php echo $user_array['profile_pic']; ?>" alt="No hay imagen">
+		<div class="profile_info">
+			<p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
+			<p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
+			<p><?php echo "Friends: " . $num_friends; ?></p>
+		</div>
 	</div>
 
-
+	<div class="main_column column">
+		<?php echo $username; ?>
 	</div>
 </body>
 </html>
